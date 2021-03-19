13ca505f7808abeca72e6162b93628455a16e3ff
condroagung <condroputro11@gmail.com>
2021-03-19 13:58:18 +0800

    penambahan

--- app/Views/dashboard_admin.php
+++ app/Views/dashboard_admin.php
@@ -223,7 +223,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Tambah Siswa Baru ada</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Tambah Siswa Baru</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
