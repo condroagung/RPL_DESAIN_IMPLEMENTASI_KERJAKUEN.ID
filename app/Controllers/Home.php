410d69a40be1f5dde23623ed13fa183824b3529d
condroagung <condroputro11@gmail.com>
2021-03-19 13:23:43 +0800

    menambahkan folder app

---
+++ app/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	public function dashboard_admin()
+	{
+		return view('dashboard_admin');
+	}
+
+	//--------------------------------------------------------------------
+
+}
